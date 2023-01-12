b24cda9c79843f74c5914a07acc75349cb63b7ab
hexuben <109147909+hexuben@users.noreply.github.com>
2023-01-12 10:24:13 +0800

    Create control.js

---
+++ f/control.js
@@ -0,0 +1,26 @@
+var TopanSystem={
+    loadfile: function(url,func){
+        $.ajax({
+            url: url,
+            success: func
+        });
+    }
+}
+
+function topan_init_all(){
+    topan_init_tabs();
+}
+$(document).ready(function(){
+    topan_init_all();
+});
+
+function topan_init_tabs(){
+    $(".topan-tab li").click(function(){
+        if(!$(this).hasClass(".topan-tab-showed")){
+            $(this.parentElement).children(".topan-tab-showed").removeClass("topan-tab-showed");
+            $(this).addClass("topan-tab-showed");
+            $(this.parentElement.parentElement).children(".topan-tab-showed").removeClass("topan-tab-showed");
+            $(this.parentElement.parentElement.children.item($(this).index()+1)).addClass("topan-tab-showed");
+        }
+    });
+}
